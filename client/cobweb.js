8ba8b7307d48934493ba02691bf394197c2ce168
alexta <terpyakov@gmail.com>
2015-12-23 11:07:12 +0300

    code review

--- client/cobweb.js
+++ client/cobweb.js
@@ -1,20 +1,11 @@
 Template.cobweb.onRendered( function() {
 	var svg = d3.select("svg");
-	var web = d3.layout.force()
-		.linkStrength(1.0)
-		.friction(.9)
-		.linkDistance(-10)
-		.charge(-50)
-		.gravity(.07)
-		.theta(0.8)
-		.alpha(0.1)
-		.on("tick", tick);
+	var web = d3.layout.force().linkStrength(1.0).friction(0.9).linkDistance(-10).charge(-50).gravity(0.07).theta(0.8).alpha(0.1).on("tick", tick);
 	var webLinks = [];
 	var webParticles = [];
 	var underlay = svg.append("g");
 	var rect = underlay.selectAll("rect").data([1]);
 	var colorRect = "#454545";
-	rect.enter().append("rect").style("fill", colorRect);
 	var depth = 5;
 	var segments = 20;
 	var n = segments * depth;
@@ -25,52 +16,37 @@ Template.cobweb.onRendered( function() {
 	var points = d3.range(n + 1);
 	var height = window.innerHeight;
 	var width = window.innerWidth;
+	rect.enter().append("rect").style("fill", colorRect);
 	web.size([width, height]);
 	rect.attr("width", width).attr("height", height);
 	radius = Math.min(width, height) / 2;
-	var origin = {
-		x: width / 2,
-		y: height / 2
-	};
+	var origin = {x: width / 2, y: height / 2};
 	var radiusStep = radius / n;
+
 	points.forEach(function (p, i) {
-
 		if (i !== points.length - 1) {
-
 			var angle = i * step;
 			var shrinkingRadius = radius - radiusStep * i;
 
 			webParticles.push({
 				x: origin.x + Math.cos(angle) * shrinkingRadius,
 				y: origin.y + Math.sin(angle) * shrinkingRadius,
-				fixed: (i < segments) && (i % 3 == 0)
+				fixed: (i < segments) && (i % 3 === 0)
 			});
 
 			if (i < points.length - 1 && i + 1 !== points.length - 1)
-				webLinks.push({
-					source: i,
-					target: i + 1
-				});
+				webLinks.push({source: i, target: i + 1});
 
 			var offnow = i + segments;
 
-			if (offnow < n - 1) webLinks.push({
-				source: i,
-				target: offnow
-			});
-			else webLinks.push({
-				source: i,
-				target: n - 1
-			});
+			if (offnow < n - 1) 
+				webLinks.push({source: i, target: offnow});
+			else
+				webLinks.push({source: i, target: n - 1});
 		}
-
 	});
 
-	webLinks.push({
-		source: 0,
-		target: segments - 1
-	});
-
+	webLinks.push({source: 0, target: segments - 1});
 	var drag = web.drag();
 	var svgWebLinks = underlay.selectAll("line").data(webLinks);
 	svgWebLinks.enter().append("line");
@@ -79,13 +55,13 @@ Template.cobweb.onRendered( function() {
 	web.nodes(webParticles).links(webLinks).start();
 
 	function tick() {
-		svgWebLinks.attr('x1', function (d) {
+		svgWebLinks.attr("x1", function (d) {
 			return d.source.x
-		}).attr('y1', function (d) {
+		}).attr("y1", function (d) {
 			return d.source.y
-		}).attr('x2', function (d) {
+		}).attr("x2", function (d) {
 			return d.target.x
-		}).attr('y2', function (d) {
+		}).attr("y2", function (d) {
 			return d.target.y
 		});
 
